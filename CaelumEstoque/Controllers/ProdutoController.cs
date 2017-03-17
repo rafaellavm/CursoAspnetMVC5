767f4c24a78655d4f8d7d97808ec0c37dfb6013b
Rafaela <rafaela.vm@gmail.com>
2017-03-17 11:57:33 -0300

    Controle da Requisição e listagem dos produtos. Lidando com formulário de e o cadastro de produtos. 5. Validando um produto com Model Validation.

---
+++ CaelumEstoque/Controllers/ProdutoController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+
+namespace CaelumEstoque.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        public ActionResult Index()
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            IList<Produto> produtos = dao.Lista();
+            ViewBag.Produtos = produtos;
+
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            CategoriasDAO categoriasDao = new CategoriasDAO();
+            IList<CategoriaDoProduto> categorias = categoriasDao.Lista();
+            ViewBag.Categorias = categorias;
+            ViewBag.Produto = new Produto();
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adiciona(Produto produto)
+        {
+            int idDaInformatica = 1;
+            if(produto.CategoriaId.Equals(idDaInformatica) && produto.Preco < 100){
+
+                //"produto.Invalido": chave que é chamada na view
+                ModelState.AddModelError("produto.Invalido", "Iformática com preço abaixo de 100 reais");
+            }
+            
+            if (ModelState.IsValid)
+            {
+                ProdutosDAO dao = new ProdutosDAO();
+                dao.Adiciona(produto);
+                //mandar o usuário para o index de Produto(controller)
+                return RedirectToAction("Index", "Produto");
+            }
+            else
+            {
+                //pra deixar os campos preenchidos
+                ViewBag.Produto = produto;
+                CategoriasDAO categoriasdao = new CategoriasDAO();
+                ViewBag.Categorias = categoriasdao.Lista();
+
+                return View("Form");
+            }
+
+        }
+    }
+}
---
+++ CaelumEstoque/README.md
@@ -0,0 +1,2 @@
+# CursoAspnetMVC5
+Curso de Asp net MVC 5, Visual Studio 2013, Framework 4.5. Cadastro de produtos. 
